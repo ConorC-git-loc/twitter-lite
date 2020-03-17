fc2ded6822296c375bcaaa146f20a1c180057807
Conor-commits <conor.curran@solv-edtech.com>
2020-03-17 11:13:59 +0000

    3rd commit, added model relationships and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :tweets
+  has_many :followers, :class_name => "Relationship", :foreign_key => "user_id"
+  has_many :following, :class_name => "Relationship", :foreign_key => "follower_id"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_105327) do
+ActiveRecord::Schema.define(version: 2020_03_17_111144) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "tweet_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_105327) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "follower_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
