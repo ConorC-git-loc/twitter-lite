9ff4da2b9a9bd349a8452e91d9535864e421cf0e
Conor-commits <conor.curran@solv-edtech.com>
2020-03-17 14:42:15 +0000

    4th commit, added some views and controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  match 'users/:id' => 'home#show', via: :get
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_111144) do
+ActiveRecord::Schema.define(version: 2020_03_17_111848) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "tweet_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_111144) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "follower_id"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
