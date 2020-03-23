4c448939e6e91108f1ee607ec37bbb5575cf4e2d
Conor-commits <conor.curran@solv-edtech.com>
2020-03-23 00:54:03 +0000

    switch to postgreSQL from SQlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_20_044119) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
